fbd977d53af4e8df45d59f629ab9d779b8fdba4c
Amanuel Fikremariam <amanfikre523@gmail.com>
2025-12-18 08:53:42 +0300

    Fix responder app lint config for eslint 9

--- responder-app/eslint.config.js
+++ responder-app/eslint.config.js
@@ -1,4 +1,4 @@
-import js from 'eslint/js';
+import js from '@eslint/js';
 import globals from 'globals';
 import reactHooks from 'eslint-plugin-react-hooks';
 import reactRefresh from 'eslint-plugin-react-refresh';
@@ -18,6 +18,10 @@ export default defineConfig([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
     },
   },
 ]);
